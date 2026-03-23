823f13d6070d7541e3acee604a11857fd58336f2
kartikmorya860@gmail.com <kartikmorya860@gmail.com>
2026-03-23 23:21:45 +0530

    first commit

--- floor_in_sorted_arr.py
+++ floor_in_sorted_arr.py
@@ -1,15 +1,16 @@
-class Solution:
-    def findFloor(self, arr, x):
-        low, high = 0, len(arr) - 1
-        result = -1
+# class Solution:
+#     def findFloor(self, arr, x):
+#         low, high = 0, len(arr) - 1
+#         result = -1
 
-        while low <= high:
-            mid = (low + high) // 2
+#         while low <= high:
+#             mid = (low + high) // 2
 
-            if arr[mid] <= x:
-                result = mid   
-                low = mid + 1  
-            else:
-                high = mid - 1
+#             if arr[mid] <= x:
+#                 result = mid   
+#                 low = mid + 1  
+#             else:
+#                 high = mid - 1
 
-        return result
+#         return result
+
